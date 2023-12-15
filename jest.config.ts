1beb81b4b35d1a7059545a15d3e49a7be5c8fa8c
cirko0 <cirko007ivan@gmail.com>
2023-12-15 09:34:38 +0100

    Adding env to our tests and renamed tests folder to test

--- jest.config.ts
+++ jest.config.ts
@@ -1,7 +1,7 @@
 import type { Config } from "@jest/types";
 
 const baseDir = "<rootDir>/src/app/server_app";
-const baseTestDir = "<rootDir>/src/tests/server_app3";
+const baseTestDir = "<rootDir>/src/test/server_app3";
 
 const config: Config.InitialOptions = {
   preset: "ts-jest",
@@ -11,6 +11,7 @@ const config: Config.InitialOptions = {
   collectCoverage: true,
   collectCoverageFrom: [`${baseDir}/**/*.ts`],
   testMatch: [`${baseTestDir}/**/*test.ts`],
+  setupFiles: ["<rootDir>/src/test/server_app3/utils/config.ts"],
 };
 
 export default config;
